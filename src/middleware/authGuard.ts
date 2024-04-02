577cb689acaafec8241fa49c0c283f7fe18bdcf8
gaisdav <gais.davletkildin@gmail.com>
2024-04-02 10:07:46 +0200

    Refactor authGuard middleware to remove unnecessary async declaration

--- src/middleware/authGuard.ts
+++ src/middleware/authGuard.ts
@@ -2,7 +2,7 @@ import { type IRequest, type IResponse } from '../common/types';
 import { type MiddlewareNext } from 'hyper-express';
 import { ForbiddenException } from '../exceptions/ForbiddenException';
 
-export const authGuard = async (_: IRequest, res: IResponse, next: MiddlewareNext): Promise<void> => {
+export const authGuard = (_: IRequest, res: IResponse, next: MiddlewareNext): void => {
   const userId = res.locals.userId;
 
   if (!userId) {

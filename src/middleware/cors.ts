aba0aebd16cf50e7cbdb68cd979c8630a4d3f2dc
gaisdav <gais.davletkildin@gmail.com>
2024-07-23 22:22:17 +0200

    changed Access-Control-Allow-Methods

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -10,10 +10,7 @@ const cors = (options: CORSOptions) => {
   return async (request: HyperExpress.Request, response: HyperExpress.Response) => {
     response.header('vary', 'Origin');
     response.header('Access-Control-Allow-Headers', 'content-type');
-    response.header(
-      'Access-Control-Allow-Methods',
-      'POST, GET, PUT, DELETE, PATCH, HEAD, CONNECT, TRACE, PURGE',
-    );
+    response.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH');
     response.header('Access-Control-Allow-Origin', options.origin);
     response.header('Access-Control-Allow-Credentials', options.credentials.toString());
 

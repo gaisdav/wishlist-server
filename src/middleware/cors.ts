240a51797cd7d3495b8e0ff38fbefb5924bde247
gaisdav <gais.davletkildin@gmail.com>
2024-08-19 10:06:47 +0200

    disabled options cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -10,7 +10,7 @@ export const cors = (options: CORSOptions) => {
   return async (request: HyperExpress.Request, response: HyperExpress.Response) => {
     response.header('vary', 'Origin');
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    response.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH');
+    response.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE, PATCH');
     response.header('Access-Control-Allow-Origin', options.origin);
     response.header('Access-Control-Allow-Credentials', options.credentials.toString());
 
